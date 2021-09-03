1589ca926621aa477ca5013a01ee6355f8ea5f59
qyt <486179@qq.com>
2021-09-03 10:24:15 +0800

    增加snmp 管理端例子 增加 系统文件处理例子

---
+++ XEngine_RfcComponents/RfcComponents_APPSnmp/RfcComponents_APPSnmp.cpp
@@ -0,0 +1,80 @@
+﻿#ifdef _WINDOWS
+#include <Windows.h>
+#include <tchar.h>
+#pragma comment(lib,"Ws2_32.lib")
+#pragma comment(lib,"../../../XEngine/XEngine_SourceCode/Debug/XClient_Socket.lib")
+#pragma comment(lib,"../../../XEngine/XEngine_SourceCode/Debug/RfcComponents_SnmpProtocol.lib")
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../../XEngine/XEngine_SourceCode/XEngine_CommHdr.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_Client/XClient_Socket/XClient_Define.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_Client/XClient_Socket/XClient_Error.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_RfcComponents/RfcComponents_SnmpProtocol/SnmpProtocol_Define.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_RfcComponents/RfcComponents_SnmpProtocol/SnmpProtocol_Error.h"
+
+//g++ -std=c++17 -Wall -g RfcComponents_APPSnmp.cpp -o RfcComponents_APPSnmp.exe -L ../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_BaseLib -L ../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_Client -L ../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_RfcComponents -lXEngine_BaseLib -lXClient_Socket -lRfcComponents_SnmpProtocol -Wl,-rpath=../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_BaseLib:../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_Client:../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_NetHelp:../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_RfcComponents,--disable-new-dtags
+
+int main()
+{
+#ifdef _WINDOWS
+	WSADATA st_WSAData;
+	WSAStartup(MAKEWORD(2, 2), &st_WSAData);
+#endif
+	SOCKET hSocket;
+	int nMsgLen = 0;
+	int nPort = 161;
+	TCHAR tszMsgBuffer[1024];
+	LPCTSTR lpszSnmpAddr = "192.168.1.9";
+	LPCTSTR lpszOIDStr = "1.3.6.1.2.1.1.1";
+	RFCSNMP_PROTOCOL st_SNMPProtocol;
+
+	memset(tszMsgBuffer, '\0', sizeof(tszMsgBuffer));
+	memset(&st_SNMPProtocol, '\0', sizeof(RFCSNMP_PROTOCOL));
+
+	st_SNMPProtocol.byVersion = XENGINE_RFCCOMPONENTS_SNMP_PROTOCOL_V3;
+	st_SNMPProtocol.byOPCode = XENGINE_SNMP_PROTOCOL_BER_TYPE_GET;
+	strcpy(st_SNMPProtocol.tszCommname, "public");
+
+	st_SNMPProtocol.st_PacketInfo.dwSerial = 698329327;
+
+	RfcComponents_SnmpHelp_StrToOID(lpszOIDStr, st_SNMPProtocol.st_BindVar.tszOIDStr, &st_SNMPProtocol.st_BindVar.nOLen);
+	RfcComponents_SnmpPacket_Protocol(tszMsgBuffer, &nMsgLen, &st_SNMPProtocol);
+
+	if (!XClient_UDPSelect_Create(&hSocket, lpszSnmpAddr, nPort))
+	{
+		printf("XClient_UDPSelect_Create:%lX\n", XClient_GetLastError());
+		return -1;
+	}
+	if (!XClient_UDPSelect_Bind(hSocket, nPort + 10000))
+	{
+		printf("XClient_UDPSelect_Bind:%lX\n", XClient_GetLastError());
+		return -1;
+	}
+	if (!XClient_UDPSelect_SendMsg(hSocket, tszMsgBuffer, nMsgLen))
+	{
+		printf("XClient_UDPSelect_SendMsg:%lX\n", XClient_GetLastError());
+		return -1;
+	}
+	nMsgLen = sizeof(tszMsgBuffer);
+	memset(tszMsgBuffer, '\0', sizeof(tszMsgBuffer));
+	memset(&st_SNMPProtocol, '\0', sizeof(RFCSNMP_PROTOCOL));
+	if (!XClient_UDPSelect_RecvMsg(hSocket, tszMsgBuffer, &nMsgLen, NULL, FALSE))
+	{
+		printf("XClient_UDPSelect_RecvMsg:%lX\n", XClient_GetLastError());
+		return -1;
+	}
+	
+	int nOLen = 0;
+	TCHAR tszOIDBuffer[128];
+	memset(tszOIDBuffer, '\0', sizeof(tszOIDBuffer));
+	RfcComponents_SnmpParse_Protocol(tszMsgBuffer, nMsgLen, &st_SNMPProtocol);
+	RfcComponents_SnmpHelp_OIDToStr(st_SNMPProtocol.st_BindVar.tszOIDStr, st_SNMPProtocol.st_BindVar.nOLen, tszOIDBuffer, &nOLen);
+
+	XClient_UDPSelect_Close(hSocket);
+#ifdef _WINDOWS
+	WSACleanup();
+#endif
+	return 0;
+}
---
+++ XEngine_SystemSdk/SystemApi_APPFile/SystemApi_APPFile.cpp
@@ -0,0 +1,54 @@
+﻿#ifdef _WINDOWS
+#include <Windows.h>
+#include <tchar.h>
+#pragma comment(lib,"../../../XEngine/XEngine_SourceCode/Debug/XEngine_BaseLib.lib")
+#pragma comment(lib,"../../../XEngine/XEngine_SourceCode/Debug/XEngine_SystemApi.lib")
+#else
+
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../../XEngine/XEngine_SourceCode/XEngine_CommHdr.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_Lib/XEngine_BaseLib/BaseLib_Define.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_Lib/XEngine_BaseLib/BaseLib_Error.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_SystemSdk/XEngine_ProcSdk/ProcFile_Define.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_SystemSdk/XEngine_SystemApi/SystemApi_Define.h"
+#include "../../../XEngine/XEngine_SourceCode/XEngine_SystemSdk/XEngine_SystemApi/SystemApi_Error.h"
+
+//g++ -std=c++17 -Wall -g SystemApi_APPFile.cpp -o SystemApi_APPFile.exe -L ../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_BaseLib -L ../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_SystemSdk -lXEngine_BaseLib -lXEngine_SystemApi -Wl,-rpath=../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_BaseLib:../../../XEngine/XEngine_Release/XEngine_Linux/Ubuntu/XEngine_SystemSdk,--disable-new-dtags
+
+
+int main()
+{
+#ifdef _WINDOWS
+	LPCTSTR lpszFile = _T("D:\\xengine_apps\\Debug\\1.txt");
+#else
+	LPCTSTR lpszFile = _T("1.txt");
+#endif
+
+	if (!SystemApi_File_CreateSparseFile(lpszFile, 10240))
+	{
+		return -1;
+	}
+
+	FILE* pSt_FileStart = fopen(lpszFile, _T("rb+"));
+	if (NULL == pSt_FileStart)
+	{
+		return -1;
+	}
+
+	FILE* pSt_FileEnd = fopen(lpszFile, _T("rb+"));
+	if (NULL == pSt_FileEnd)
+	{
+		return -1;
+	}
+	fseek(pSt_FileEnd, 5, SEEK_CUR);
+
+	int nRet = fwrite("01234", 1, 5, pSt_FileStart);
+	nRet = fwrite("56789", 1, 5, pSt_FileEnd);
+
+	fclose(pSt_FileStart);
+	fclose(pSt_FileEnd);
+	return 0;
+}
