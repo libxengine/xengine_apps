a56e61f336f9a80d2606cfa68e163aafb9eb5f44
qyt <486179@qq.com>
2026-09-02 15:32:32 +0800

    modify:match xengine v9.42

--- XEngine_BaseLib/BaseLib_APPSafe/BaseLib_APPSafe.cpp
+++ XEngine_BaseLib/BaseLib_APPSafe/BaseLib_APPSafe.cpp
@@ -1,4 +1,5 @@
 ﻿#include "../../XEngine_ExampleHdr.h"
+#define _CRT_SECURE_NO_WARNINGS
 #ifdef _MSC_BUILD
 #include <Windows.h>
 #include <tchar.h>
@@ -32,8 +33,28 @@
 
 //Linux MacOS:g++ -std=c++20 -Wall -g Lib_APPSafe.cpp -o Lib_APPSafe.exe -lXEngine_BaseSafe
 
+// 引入不同平台所需的头文件
+#if defined(_WIN32) || defined(_WIN64)
+#include <io.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#else
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#endif
+
+void SystemApi_OpenFileSecure(const char* lpszFileName, const char* mode)
+{
+	FILE* pSt_File = BaseSafe_File_CreateA(lpszFileName, mode);
+	fwrite("123", 1, 3, pSt_File);
+	fclose(pSt_File);
+}
+
 int main()
 {
+	SystemApi_OpenFileSecure("D:\\xengine_apps\\1.txt", "wb");
+
 	XCHAR tszDestStr[8] = {};
 	LPCXSTR lpszSourceStr = _X("123456789");
 
